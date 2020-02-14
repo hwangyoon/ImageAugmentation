b0bedd9edc53e9948f157642097f1519cbc57638
hwanglight <stray.polina.study@gmail.com>
2020-02-14 15:21:30 +0300

    Add  CLI
    There are two commands: Hi and exit

--- CLI/main.cpp
+++ CLI/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string command;
+    // TODO: Узнать, что конкретно надо в интерфейсе, и написать это
+    while (true) {
+        cin >> command;
+        if (command == "exit")
+            break;
+        if (command == "Hi") {
+            cout << "Hello!!!! \nI'm really glad to meet you!\n";
+            continue;
+        }
+    }
     return 0;
 }
