1aa957ab3f240ceb54dc77188df1be5c5b255303
LenSunko01 <sunko.elena2001@gmail.com>
2020-02-20 18:16:39 +0300

    Adding unit test for save function

--- Test_QImage/Test_Image.cpp
+++ Test_QImage/Test_Image.cpp
@@ -30,4 +30,16 @@ void Test_Image::test_color_methods() {
     described as a standard global color*/
     img.fill(Qt::white);
     QCOMPARE(img.pixel(QPoint(1, 1)), qRgb(255, 255, 255));
+    QTemporaryDir dir;
+    if (dir.isValid()) {
+        /*save takes filename, format, quality parameters
+        if format = 0 QImage will guess the format
+        quality in range 0 to 100 or -1(the default)*/
+        bool success_save = img.save(dir.path() + "/new.png", 0, -1);
+        QCOMPARE(success_save, true);
+        QImage new_img;
+        bool success_load = new_img.load(dir.path() + "/new.png");
+        QCOMPARE(success_load, true);
+        QCOMPARE(new_img, img);
+    }
 }
--- Test_QImage/Test_QImage.pro
+++ Test_QImage/Test_QImage.pro
@@ -15,7 +15,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-        Test_Image.cpp
+    Test_Image.cpp
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
