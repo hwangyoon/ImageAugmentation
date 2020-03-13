3bda1cfae781e13d8241463b604038a60f77dfde
hwanglight <stray.polina.study@gmail.com>
2020-03-13 17:17:02 +0300

    Changed gui ^^

--- guidir/mainwindow/mainwindow.h
+++ guidir/mainwindow/mainwindow.h
@@ -5,7 +5,6 @@
 #include <QPlainTextEdit>
 #include <QListWidget>
 #include <QTextBrowser>
-#include "../guicontroller/guicontroller.h"
 
 namespace Ui {
 class MainWindow;
@@ -24,16 +23,11 @@ private slots:
 
     void on_pushButton_process_clicked();
 
-    void on_pushButton_preview_clicked();
-
 private:
     Ui::MainWindow *ui;
     void view_algo();
     void add_algo(const QStringList& list);
     QList<QListWidgetItem *> find_selected_items(QListWidget &list);
-    QList<QListWidgetItem *> process_list(QListWidget &list);
-    GuiController controller;
-    QStringList make_txt_list(QList<QListWidgetItem*> list);
 };
 
 #endif // MAINWINDOW_H
