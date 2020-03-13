dc397a3b6b53763cbb1af00353853f8a7c0df0f3
lizazhemchuzhina <34946269+lizazhemchuzhina@users.noreply.github.com>
2020-03-13 15:00:09 +0300

    Update rotate90.h

--- main/include/rotate90.h
+++ main/include/rotate90.h
@@ -2,13 +2,13 @@
 #ifndef ROTATE90CLOCKWISE_H
 #define ROTATE90CLOCKWISE_H
 #include "algo.h"
-enum direction90 {CLOCKWISE90,COUNTERCLOCKWISE90};
+enum Direction90 {CLOCKWISE90,COUNTERCLOCKWISE90};
 class Rotate90: public Algorithm {
 public:
     QImage processImage(QImage *workingModel);
     Rotate90(){direct = CLOCKWISE90;}
-    Rotate90(direction90 d):direct(d) { }
+    Rotate90(Direction90 d):direct(d) { }
 private:
-   direction90 direct;
+   Direction90 direct;
 };
 #endif // ROTATE90CLOCKWISE_H
