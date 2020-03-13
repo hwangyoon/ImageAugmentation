9722dcc95984f366c11002624d60d64ffdfc30d5
hwanglight <stray.polina.study@gmail.com>
2020-03-13 17:17:02 +0300

    Changed gui ^^

--- guidir/mainwindow/mainwindow.cpp
+++ guidir/mainwindow/mainwindow.cpp
@@ -7,16 +7,6 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     view_algo();
-    QString info = "Привет ^^ \nКак пользоваться этим приложением?.. (мотать вниз для ответа) \n\
-                     - Чтобы загрузить картику надо ввести в первое поле адрес загружаемой картинки и нажать на кнопку LOAD \n\
-                     - Чтобы начать обработку надо ввести во второе поле место для сохранения и нажать на кнопку PROCESS \n\
-                     - Чтобы обработка не была пустым копированием надо выбрать алгоритмы в списке справа\
-(то, как они будут выглядеть можно посмотреть, нажав на кнопку PREVIEW)";
-    QTextBrowser *view_message = ui->textBrowser_info;
-    view_message->setText(info);
-    QLabel *label = ui->label_image;
-    QString what_printed = "Здесь появится Ваша картинка ^^ \n     (но сначала её стоит загрузить)";
-    label->setText(what_printed);
 }
 
 MainWindow::~MainWindow()
@@ -26,25 +16,20 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_load_clicked()
 {
-    QTextEdit *text_path_in = ui->textEdit_path_in;
-    QString path_in = text_path_in->toPlainText();
-    text_path_in->setText("");
+    QTextEdit *text_path = ui->textEdit_path;
+    QString path = text_path->toPlainText();
+    text_path->setText("");
     // Тут надо path передать как путь Controller'у, он даст QImage
-    // Забили на это, сами загрузим
+    QImage image;
     QLabel *label = ui->label_image;
-    QPixmap *p=new QPixmap(path_in);
-    QPixmap p1(p->scaled ( 300,300, Qt::IgnoreAspectRatio, Qt::SmoothTransformation ));
-    label->setPixmap(p1); // Здесь картинка включилась (надеюсь)
-    controller.save_path_in(path_in);
-    //text_path_in->setText("DONE");
+    label->setPixmap(QPixmap::fromImage(image)); // Здесь картинка включилась (надеюсь)
 }
 
 void MainWindow::view_algo() {
     QListWidget *algo_list = ui->listWidget_algo;
 
     static QStringList LIST_ITEMS =
-        QStringList() << "Crop from middle" << "Horizontal flip" << "Rotate 45 dg" << "Rotate 90 dg" << "Vertical flip" << "Dithering" << "GaussianNoise"
-                      << "Kuwahara" << "Lightening" << "RGBTone" << "WhiteBlack" ;
+        QStringList() << "C++" << "Python" << "Java" << "C#" << "PHP" << "Ruby" << "JavaScript";
 
     add_algo(LIST_ITEMS);
 
@@ -64,45 +49,17 @@ void MainWindow::add_algo(const QStringList& list)
     }
 }
 
-QList<QListWidgetItem *> MainWindow::process_list(QListWidget &list)
+void MainWindow::on_pushButton_process_clicked()
 {
+    QListWidget *algo_list = ui->listWidget_algo;
     QString what_printed("");
-    QList<QListWidgetItem *> selectedItems = find_selected_items(list);
+    QList<QListWidgetItem *> selectedItems = find_selected_items(*algo_list);
     foreach (const QListWidgetItem* item, selectedItems) {
         what_printed += " " + item->text();
     }
-    QTextBrowser *view_message = ui->textBrowser_info;
+    // Вернёт строчки алгоритмов, надо переделать в алгоритмы и передать контроллеру?
+    QTextBrowser *view_message = ui->textBrowser_message;
     view_message->setText(what_printed);
-    return selectedItems;
-}
-
-void MainWindow::on_pushButton_process_clicked()
-{
-    //передам process_list();
-    QTextEdit *text_path_to = ui->textEdit_path_out;
-    QString path_to = text_path_to->toPlainText();
-    controller.save_path_to(path_to);
-    text_path_to->setText("");
-    controller.make_request(make_txt_list(find_selected_items(*ui->listWidget_algo)));
-}
-
-QStringList MainWindow::make_txt_list(QList<QListWidgetItem*> list) {
-    QStringList txt_list;
-    foreach(QListWidgetItem* line, list) {
-        txt_list << line->text();
-    }
-    return txt_list;
-}
-
-void MainWindow::on_pushButton_preview_clicked()
-{
-    //передам process_list();
-    QLabel *label = ui->label_image;
-    QStringList txt_algo = make_txt_list(find_selected_items(*ui->listWidget_algo));
-    QImage image = controller.make_request_preview(txt_algo);
-    QPixmap p = QPixmap::fromImage(image);
-    QPixmap p1(p.scaled ( 300,300, Qt::IgnoreAspectRatio, Qt::SmoothTransformation ));
-    label->setPixmap(p1); // Здесь картинка включилась (надеюсь)
 }
 
 QList<QListWidgetItem *> MainWindow::find_selected_items(QListWidget &list)
