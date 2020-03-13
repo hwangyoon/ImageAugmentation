b45077e8eefa191950193689d9c5daf6d28065d3
hwanglight <stray.polina.study@gmail.com>
2020-03-13 11:26:26 +0300

    GUI done (we need only connect it)  Кажется, там стоило всё же названия алгоритмов, а не яхыки программирования, но не важно

--- guidir/mainwindow.h
+++ guidir/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPlainTextEdit>
+#include <QListWidget>
+#include <QTextBrowser>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,14 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_load_clicked();
+
+    void on_pushButton_process_clicked();
+
 private:
     Ui::MainWindow *ui;
+    void view_algo();
 };
 
 #endif // MAINWINDOW_H
