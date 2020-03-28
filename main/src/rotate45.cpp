7a59339075b8f846cfa4b28e7c1c8306687cf005
lizazhemchuzhina <34946269+lizazhemchuzhina@users.noreply.github.com>
2020-03-28 23:14:26 +0300

    Update rotate45.cpp

--- main/src/rotate45.cpp
+++ main/src/rotate45.cpp
@@ -3,16 +3,16 @@
 QImage Rotate45::processImage(const QImage *workingModel) {
     int32_t cols = workingModel->width();
     int32_t rows = workingModel->height();
-    QImage rotatedPicture(rows+cols,cols+rows,workingModel->format());
+    QImage rotatedPicture(rows + cols, cols + rows, workingModel->format());
     for (int32_t i = 0; i < cols; i++) {
         for (int32_t j = 0; j < rows; j++) {
             if(direct == CLOCKWISE45) {
-                rotatedPicture.setPixel(i-j+rows, i+j, workingModel->pixel(QPoint(i,j)));
-                rotatedPicture.setPixel(i-j+rows - 1, i+j, workingModel->pixel(QPoint(i,j)));
+                rotatedPicture.setPixel(i - j + rows, i + j, workingModel->pixel(QPoint(i,j)));
+                rotatedPicture.setPixel(i - j + rows - 1, i + j, workingModel->pixel(QPoint(i,j)));
             }
             else {
-                rotatedPicture.setPixel(i+j,j-i+cols, workingModel->pixel(QPoint(i,j)));
-                rotatedPicture.setPixel(i+j,j-i+cols-1,workingModel->pixel(QPoint(i,j)));
+                rotatedPicture.setPixel(i + j,j - i + cols, workingModel->pixel(QPoint(i,j)));
+                rotatedPicture.setPixel(i + j,j - i + cols - 1, workingModel->pixel(QPoint(i,j)));
             }
         }
     }
@@ -24,7 +24,7 @@ Rotate45Builder::Rotate45Builder() {
 }
 
 Rotate45Builder Rotate45Builder::setDirection(Direction45 d) {
-    this->direct=d;
+    this->direct = d;
     return *this;
 }
 
