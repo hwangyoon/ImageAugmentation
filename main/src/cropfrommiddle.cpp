91bc290c609d485f955b0b3cc58aeb3355cc4a9b
lizazhemchuzhina <34946269+lizazhemchuzhina@users.noreply.github.com>
2020-03-28 23:13:02 +0300

    Update cropfrommiddle.cpp

--- main/src/cropfrommiddle.cpp
+++ main/src/cropfrommiddle.cpp
@@ -1,14 +1,13 @@
 #include "../include/cropfrommiddle.h"
-#include <iostream>
 QImage CropGivenPiece::processImage(const QImage *workingModel) {
-    startXInPercent = startXInPercent*workingModel->width()/100.0;
-    startYInPercent = startYInPercent*workingModel->height()/100.0;
-    colsInPercent = colsInPercent*workingModel->width()/100.0 - startYInPercent;
-    rowsInPercent = rowsInPercent*workingModel->height()/100.0 - startXInPercent;
+    startXInPercent = startXInPercent * workingModel->width() / 100.0;
+    startYInPercent = startYInPercent * workingModel->height() / 100.0;
+    colsInPercent = colsInPercent * workingModel->width() / 100.0 - startYInPercent;
+    rowsInPercent = rowsInPercent * workingModel->height() / 100.0 - startXInPercent;
 
     QImage croppedImage(colsInPercent, rowsInPercent, workingModel->format());
-    for (int32_t i = 0; i < colsInPercent-1; i++) {
-        for (int32_t j = 0; j < rowsInPercent-1; j++) {
+    for (int32_t i = 0; i < colsInPercent - 1; i++) {
+        for (int32_t j = 0; j < rowsInPercent - 1; j++) {
             croppedImage.setPixel(i, j, workingModel->pixel(QPoint(i + startXInPercent, j + startYInPercent)));
         }
     }
@@ -16,22 +15,22 @@ QImage CropGivenPiece::processImage(const QImage *workingModel) {
 }
 
 CropGivenPieceBuilder CropGivenPieceBuilder::setX(int x) {
-    this->startXInPercent=x;
+    this->startXInPercent = x;
     return *this;
 }
 
 CropGivenPieceBuilder CropGivenPieceBuilder::setY(int y) {
-    this->startYInPercent=y;
+    this->startYInPercent = y;
     return *this;
 }
 
 CropGivenPieceBuilder CropGivenPieceBuilder::setCols(int cols) {
-    this->colsInPercent=cols;
+    this->colsInPercent = cols;
     return *this;
 }
 
 CropGivenPieceBuilder CropGivenPieceBuilder::setRows(int rows) {
-    this->rowsInPercent=rows;
+    this->rowsInPercent = rows;
     return *this;
 }
 
@@ -51,13 +50,13 @@ CropGivenPiece CropGivenPieceBuilder::build() {
         this->colsInPercent = this->DEFAULT_colsInPercent;
         this->rowsInPercent = this->DEFAULT_rowsInPercent;
     }
-    if(count>0 && count<4)
+    if(count > 0 && count < 4)
         throw std::invalid_argument("Few arguments");
 
-    if((this->startXInPercent<0 || this->startXInPercent>100)||
-           (this->startYInPercent<0 || this->startYInPercent>100)||
-           (this->colsInPercent<0 || this->colsInPercent>100)||
-           (this->rowsInPercent<0 || this->rowsInPercent>100))
+    if((this->startXInPercent < 0 || this->startXInPercent > 100)||
+           (this->startYInPercent < 0 || this->startYInPercent > 100)||
+           (this->colsInPercent < 0 || this->colsInPercent > 100)||
+           (this->rowsInPercent < 0 || this->rowsInPercent > 100))
         throw std::out_of_range("Invalid input: please set field in 0...100");
     CropGivenPiece *algo = new CropGivenPiece();
     algo->startXInPercent = this->startXInPercent.toInt();
