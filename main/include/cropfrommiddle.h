92c5b809b12a12eaec219902590576ca2350a6a7
lizazhemchuzhina <34946269+lizazhemchuzhina@users.noreply.github.com>
2020-03-28 23:08:50 +0300

    Update cropfrommiddle.h

--- main/include/cropfrommiddle.h
+++ main/include/cropfrommiddle.h
@@ -6,9 +6,9 @@
 #include <exception>
 class CropGivenPiece: public Algorithm {
 private:
-    int32_t startXInPercent,startYInPercent;//coordinates in percent of upper left corner
-    int32_t colsInPercent,rowsInPercent;//width and height in percent of cropping piece
-    CropGivenPiece()=default;
+    int32_t startXInPercent, startYInPercent;//coordinates in percent of upper left corner
+    int32_t colsInPercent, rowsInPercent;//width and height in percent of cropping piece
+    CropGivenPiece() = default;
 public:
     QImage processImage(const QImage *workingModel);
     friend class CropGivenPieceBuilder;
@@ -16,12 +16,12 @@ public:
 
 class CropGivenPieceBuilder {
 private:
-    QVariant startXInPercent,startYInPercent;//coordinates in percent of upper left corner
-    QVariant colsInPercent,rowsInPercent;//width and height in percent of cropping piece
-    const int32_t DEFAULT_startXInPercent = 40,DEFAULT_startYInPercent = 40;
-    const int32_t DEFAULT_colsInPercent = 70,DEFAULT_rowsInPercent = 70;
+    QVariant startXInPercent, startYInPercent;//coordinates in percent of upper left corner
+    QVariant colsInPercent, rowsInPercent;//width and height in percent of cropping piece
+    const int32_t DEFAULT_startXInPercent = 40, DEFAULT_startYInPercent = 40;
+    const int32_t DEFAULT_colsInPercent = 70, DEFAULT_rowsInPercent = 70;
 public:
-    CropGivenPieceBuilder()=default;
+    CropGivenPieceBuilder() = default;
     CropGivenPieceBuilder setX(int startXInPercent);
     CropGivenPieceBuilder setY(int startYInPercent);
     CropGivenPieceBuilder setCols(int colsInPercent);
