a3659ef1336423121c885df8bec54c849b3c3a01
lizazhemchuzhina <34946269+lizazhemchuzhina@users.noreply.github.com>
2020-03-28 23:15:12 +0300

    Update rotate90.cpp

--- main/src/rotate90.cpp
+++ main/src/rotate90.cpp
@@ -3,13 +3,13 @@
 QImage Rotate90::processImage(const QImage *workingModel) {
     int32_t cols = workingModel->width();
     int32_t rows = workingModel->height();
-    QImage rotatedPicture(rows,cols,workingModel->format());
+    QImage rotatedPicture(rows, cols, workingModel->format());
     for (int32_t i = 0; i < rows; i++) {
         for (int32_t j = 0; j < cols; j++) {
             if(direct == CLOCKWISE90)
-                rotatedPicture.setPixel(i, j, workingModel->pixel(QPoint(j,rows-i-1)));
+                rotatedPicture.setPixel(i, j, workingModel->pixel(QPoint(j,rows - i - 1)));
             else
-                rotatedPicture.setPixel(i, j, workingModel->pixel(QPoint(cols-1-j,i)));
+                rotatedPicture.setPixel(i, j, workingModel->pixel(QPoint(cols - 1 - j, i)));
         }
     }
 
@@ -20,7 +20,7 @@ Rotate90Builder::Rotate90Builder() {
     direct = CLOCKWISE90;
 }
 Rotate90Builder Rotate90Builder::setDirection(Direction90 d) {
-    this->direct=d;
+    this->direct = d;
     return *this;
 }
 
