7195189bf83d5b0d6ee029620526e58cec45862d
lizazhemchuzhina <34946269+lizazhemchuzhina@users.noreply.github.com>
2020-03-29 15:43:00 +0300

    Revert "Builder"

--- main/src/cropfrommiddle.cpp
+++ main/src/cropfrommiddle.cpp
@@ -1,68 +1,67 @@
 #include "../include/cropfrommiddle.h"
 QImage CropGivenPiece::processImage(const QImage *workingModel) {
-    upperLeftXInPercent = upperLeftXInPercent * workingModel->width() / 100.0;
-    upperLeftYInPercent = upperLeftYInPercent * workingModel->height() / 100.0;
-    downRightXInPercent = downRightXInPercent * workingModel->width() / 100.0 - upperLeftXInPercent;
-    downRightYInPercent = downRightYInPercent * workingModel->height() / 100.0 - upperLeftYInPercent;
+    startXInPercent = startXInPercent * workingModel->width() / 100.0;
+    startYInPercent = startYInPercent * workingModel->height() / 100.0;
+    colsInPercent = colsInPercent * workingModel->width() / 100.0 - startYInPercent;
+    rowsInPercent = rowsInPercent * workingModel->height() / 100.0 - startXInPercent;
 
-    QImage croppedImage(downRightXInPercent - 1, downRightYInPercent - 1, workingModel->format());
-    for (int32_t i = 0; i < downRightXInPercent - 1; i++) {
-        for (int32_t j = 0; j < downRightYInPercent - 1; j++) {
-            croppedImage.setPixel(i, j, workingModel->pixel(QPoint(i + upperLeftXInPercent, j + upperLeftYInPercent)));
+    QImage croppedImage(colsInPercent, rowsInPercent, workingModel->format());
+    for (int32_t i = 0; i < colsInPercent - 1; i++) {
+        for (int32_t j = 0; j < rowsInPercent - 1; j++) {
+            croppedImage.setPixel(i, j, workingModel->pixel(QPoint(i + startXInPercent, j + startYInPercent)));
         }
     }
     return croppedImage;
 }
 
 CropGivenPieceBuilder CropGivenPieceBuilder::setX(int x) {
-    this->upperLeftXInPercent = x;
+    this->startXInPercent = x;
     return *this;
 }
 
 CropGivenPieceBuilder CropGivenPieceBuilder::setY(int y) {
-    this->upperLeftYInPercent = y;
+    this->startYInPercent = y;
     return *this;
 }
 
 CropGivenPieceBuilder CropGivenPieceBuilder::setCols(int cols) {
-    this->downRightXInPercent = cols;
+    this->colsInPercent = cols;
     return *this;
 }
 
 CropGivenPieceBuilder CropGivenPieceBuilder::setRows(int rows) {
-    this->downRightYInPercent = rows;
+    this->rowsInPercent = rows;
     return *this;
 }
 
 CropGivenPiece CropGivenPieceBuilder::build() {
     int count = 0;
-    if(this->upperLeftXInPercent.isNull())
+    if(this->startXInPercent.isNull())
         count++;
-    if(this->upperLeftYInPercent.isNull())
+    if(this->startYInPercent.isNull())
         count++;
-    if(this->downRightXInPercent.isNull())
+    if(this->colsInPercent.isNull())
         count++;
-    if(this->downRightYInPercent.isNull())
+    if(this->rowsInPercent.isNull())
         count++;
     if(count == 4) {
-        this->upperLeftXInPercent = this->DEFAULT_upperLeftXInPercent;
-        this->upperLeftYInPercent = this->DEFAULT_upperLeftYInPercent;
-        this->downRightXInPercent = this->DEFAULT_downRightXInPercent;
-        this->downRightYInPercent = this->DEFAULT_downRightYInPercent;
+        this->startXInPercent = this->DEFAULT_startXInPercent;
+        this->startYInPercent = this->DEFAULT_startYInPercent;
+        this->colsInPercent = this->DEFAULT_colsInPercent;
+        this->rowsInPercent = this->DEFAULT_rowsInPercent;
     }
     if(count > 0 && count < 4)
         throw std::invalid_argument("Few arguments");
 
-    if((this->upperLeftXInPercent < 0 || this->upperLeftXInPercent > 100)||
-           (this->upperLeftYInPercent < 0 || this->upperLeftYInPercent > 100)||
-           (this->downRightXInPercent < 0 || this->downRightXInPercent > 100)||
-           (this->downRightYInPercent < 0 || this->downRightYInPercent > 100)||
-           (this->downRightXInPercent == 0) || (this->downRightYInPercent == 0))
+    if((this->startXInPercent < 0 || this->startXInPercent > 100)||
+           (this->startYInPercent < 0 || this->startYInPercent > 100)||
+           (this->colsInPercent < 0 || this->colsInPercent > 100)||
+           (this->rowsInPercent < 0 || this->rowsInPercent > 100))
         throw std::out_of_range("Invalid input: please set field in 0...100");
     CropGivenPiece *algo = new CropGivenPiece();
-    algo->upperLeftXInPercent = this->upperLeftXInPercent.toInt();
-    algo->upperLeftYInPercent = this->upperLeftYInPercent.toInt();
-    algo->downRightXInPercent = this->downRightXInPercent.toInt();
-    algo->downRightYInPercent = this->downRightYInPercent.toInt();
+    algo->startXInPercent = this->startXInPercent.toInt();
+    algo->startYInPercent = this->startYInPercent.toInt();
+    algo->colsInPercent = this->colsInPercent.toInt();
+    algo->rowsInPercent = this->rowsInPercent.toInt();
     return *algo;
 }
