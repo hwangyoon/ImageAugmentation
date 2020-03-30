7cb98088ff8d1c540524507e0022287c0fbfc24b
hwanglight <stray.polina.study@gmail.com>
2020-03-30 11:46:51 +0300

    Save progress Now we can load image and see it ^^

--- guidir/guicontroller/guicontroller.cpp
+++ guidir/guicontroller/guicontroller.cpp
@@ -3,3 +3,15 @@
 GuiController::GuiController()
 {
 }
+
+QImage GuiController::make_request(QStringList algo_list)
+{
+}
+QImage GuiController::make_request_preview(QStringList algo_list)
+{
+
+}
+void GuiController::save_path_in(QString new_path_in)
+{
+    path_in = new_path_in;
+}
