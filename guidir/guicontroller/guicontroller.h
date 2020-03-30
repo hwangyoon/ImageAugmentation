d9387625a90cbd7a870022abf9835b293584aeb6
hwanglight <stray.polina.study@gmail.com>
2020-03-30 11:46:51 +0300

    Save progress Now we can load image and see it ^^

--- guidir/guicontroller/guicontroller.h
+++ guidir/guicontroller/guicontroller.h
@@ -2,12 +2,22 @@
 #define GUICONTROLLER_H
 
 #include <vector>
+#include <QImage>
+#include <QString>
+#include "../../main/include/request.h"
+#include "../../main/include/manager.h"
 
 class GuiController
 {
 private:
+    std::vector<Request*> request_list;
+    AlgorithmManager manager;
+    QString path_in;
 public:
     GuiController();
+    QImage make_request(QStringList algo_list);
+    QImage make_request_preview(QStringList algo_list);
+    void save_path_in(QString new_path_in);
 };
 
 #endif // GUICONTROLLER_H
