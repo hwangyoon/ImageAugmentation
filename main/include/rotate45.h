d6d0ea9d136e7d05a0cbc47242656f5fe7c9af62
LenSunko01 <vikger@mail.ru>
2020-04-05 01:20:19 +0300

    adding blur algo

--- main/include/rotate45.h
+++ main/include/rotate45.h
@@ -1,12 +1,26 @@
 #ifndef ROTATE45_H
 #define ROTATE45_H
 #include "algo.h"
+#include <cmath>
 enum Direction45 {CLOCKWISE45,COUNTERCLOCKWISE45};
 class Rotate45: public Algorithm {
 public:
     QImage processImage(const QImage *workingModel);
     friend class Rotate45Builder;
 private:
+    QImage& blur_left_upper_angle(QImage& img,
+                                  int len, int height);
+    QImage& blur_right_upper_angle(QImage& img,
+                                  int len, int height);
+    QImage& blur_left_bottom_angle(QImage& img,
+                                  int len, int height);
+    QImage& blur_right_bottom_angle(QImage& img,
+                                  int len, int height);
+    QImage& blur(QImage& img,
+                 QPoint LeftUpperAngle,
+                 QPoint RightUpperAngle,
+                 QPoint RightBottomAngle,
+                 QPoint LeftBottomAngle);
     Direction45 direct;
     Rotate45() = default;
 };
